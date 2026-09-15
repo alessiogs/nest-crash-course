4830e12e2f298eeda431b39cc0b13030d80b14bf
alessiogs <alessio.soggiu@hotmail.it>
2026-09-15 15:40:10 +0200

    feat: add tests

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { ConfigModule } from 'src/config/config.module';
+import { EpisodesService } from 'src/episodes/episodes.service';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
+  const mockEpisodesService = {
+    findAll: () => [{ id: 'id' }],
+    findFeaturedEpisodes: () => [{ id: 'id' }],
+    findOne: () => ({ id: 'id' }),
+    create: () => ({ id: 'id' }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [EpisodesController],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService }],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
@@ -15,4 +26,11 @@ describe('EpisodesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('findOne', () =>
+    it('should return the id of the new episode', () => {
+      const id = 'id';
+      const response = controller.findOne(id);
+      expect(response).toEqual({ id: 'id' });
+    }));
 });
